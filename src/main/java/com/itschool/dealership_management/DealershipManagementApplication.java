468ccd30e5b2d44fb960e00e791674694786a26f
Lilik-mtv <lilic999@yyahoo.com>
2025-03-27 10:27:00 -0700

    Initial commit

---
+++ src/main/java/com/itschool/dealership_management/DealershipManagementApplication.java
@@ -0,0 +1,13 @@
+package com.itschool.dealership_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealershipManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealershipManagementApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/itschool/dealership_management/mapper/ObjectMapper.java
@@ -0,0 +1,8 @@
+package com.itschool.dealership_management.mapper;
+
+public interface ObjectMapper <T, R> {
+
+    T mapToDo (R r);
+
+    R mapToEntity(T t);
+}
